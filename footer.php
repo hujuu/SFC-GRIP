3011a7ba9e323bf9a103c146f815ed8d03093832
hujuu <kenakai@a5.keio.jp>
2014-06-30 22:04:12 +0900

    files

---
+++ footer.php
@@ -0,0 +1,42 @@
+
+
+
+
+                </div>	
+                <!-- Content Section ends here -->	
+                
+                                                              
+			</div>	
+			<!-- Wrapper two ends here -->	
+            
+            <!-- Footer Section starts here -->
+                <div id="footer_section">
+                    
+                                            <div id="bottomfooterwidgetcontainer">
+                                                <div class="bottomfooterwidgety">
+                                                    <h3>SFC GRIP</h3>
+                                                    <p>　ホーム　|　サイトマップ　|　コンセプト　|　運営主体　|　お問い合わせ </p>
+                                                        <div class="footerp"><?php _e('Copyright &copy; 2014 , SFC GRIP All rights reserved.', 'hujuu') ?></div>
+                                                        <?php if( is_home() || is_front_page() ): ?>
+                                                        <?php else: ?>
+                                                                       
+                                                        <?php endif;?>
+                                                </div>
+                                            </div>                  
+                        
+                 </div> 
+                 <!-- Footer Section ends here -->  
+
+
+		</div>	
+		<!-- Wrapper three ends here -->           
+	</div>	
+	<!-- Wrapper four ends here -->	            				
+	</div>	
+	<!-- Wrapper one ends here -->	
+
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php global $query_string; ?><html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head profile="http://gmpg.org/xfn/11">
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+	
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?> " type="text/css" media="all" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<!-- Wrapper one starts here -->
+	<div id="wrapper_one">	
+	<!-- Wrapper four starts here -->
+	<div id="wrapper_four">    
+
+		<!-- Wrapper one starts here -->
+		<div id="wrapper_two">
+
+        	<!-- Header socialn search starts here -->
+            <div id="header_social_cont">
+            
+            	<div id="header_social">
+                	
+                    <div class="header_rss">
+                    	<p><a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.png" width="35" height="35" border="0" alt="Rss" /></a></p>
+                        <p><a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.png" width="35" height="35" border="0" alt="Rss" /></a></p>
+                        <p><a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/google.png" width="35" height="35" border="0" alt="Rss" /></a></p>
+                    </div>
+                    <!--
+                	<div class="header_search_cont">
+                    	<?php get_search_form(); ?>
+                    </div> 
+                    -->
+
+                	<div class="header_social_icons">
+                    
+                                    <ul>
+    
+                                        <?php if(of_get_option('redit_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('redit_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/redit.png" alt="redit" /></a></li>
+                                        <?php endif; ?>
+    
+                                        <?php if(of_get_option('delicious_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('delicious_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/delicious.png" alt="delicious" /></a></li>
+                                        <?php endif; ?>
+    
+                                        <?php if(of_get_option('stumble_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('stumble_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stumble.png" alt="stumble" /></a></li>
+                                        <?php endif; ?>
+    
+                                        <?php if(of_get_option('youtube_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('youtube_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/youtube.png" alt="youtube" /></a></li>
+                                        <?php endif; ?>
+    
+                                        <?php if(of_get_option('flickr_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('flickr_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/flickr.png" alt="flickr" /></a></li>
+                                        <?php endif; ?>
+    
+                                        <?php if(of_get_option('linkedin_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('linkedin_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/linkedin.png" alt="linkedin" /></a></li>
+                                        <?php endif; ?>
+    
+                                        <?php if(of_get_option('google_id')) : ?>
+                                        <li><a href="<?php echo esc_url( of_get_option('google_id') ); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/google.png" alt="google" /></a></li>
+                                        <?php endif; ?>
+
+                                                                        
+                                    </ul>                    
+                    
+                    
+                    </div>                                       
+                
+            
+            	</div>
+            
+            </div>
+            <!-- Header socialn search starts here -->
+            
+			<!-- Wrapper three starts here -->
+			<div id="wrapper_three">
+            
+
+                 <!-- Logo Section starts here -->
+                 <div id="logo_section">
+
+                            <div id="logo">
+                                <p class="logo_title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo('name'); ?></a></p>
+                                <p class="logo_desc"><?php bloginfo('description'); ?></p>
+                            </div>
+                            
+                            
+                            
+            
+                </div>	
+                <!-- Logo Section ends here -->	                 			
+
+                  
+                <!-- Content Section starts here -->
+                <div id="content_section">
+   
+                    <!-- Menu Section starts here -->
+                    
+            			<div id="menu">
+							<!-- <?php wp_nav_menu( array( 'theme_location' => 'mainmenu', 'menu_class' => 'dropdown dropdown-horizontal','fallback_cb'     => 'Destro_backupmenu', 'menu_id'=>'Main_nav', 'container'=>'') ); ?>			
+                        -->
+                        <nav>
+            <ul class="fancyNav">
+                <li id="home"><a href="/post" class="homeIcon">Home</a></li>
+                <li id="news"><a href="/post/?page_id=2">特　集</a></li>
+                <li id="about"><a href="#about">インタビュー</a></li>
+                <li id="services"><a href="#services">起　業</a></li>
+                <li id="contact"><a href="#contact">キャリア</a></li>
+                <li id="abroad"><a href="#contact">留　学</a></li>
+                <li id="abroad"><a href="#contact">交　流</a></li>
+            </ul>
+        </nav>
+                        </div>
+ 
+
+                   
+                    <!-- Menu Section ends here -->	                                      
