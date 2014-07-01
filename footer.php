a284625c5ca9fbff995148bb5d16a5e0531915ce
hujuu <kenakai@a5.keio.jp>
2014-07-02 00:15:04 +0900

    footer mouseover

--- footer.php
+++ footer.php
@@ -3,18 +3,28 @@
 
 
                 </div>	
-                <!-- Content Section ends here -->	     
-                                                              
+                <!-- Content Section ends here -->                                                        
 			</div>	
 			<!-- Wrapper two ends here -->	
             
             <!-- Footer Section starts here -->
                 <div id="footer_section">
-                    
                                             <div id="bottomfooterwidgetcontainer">
                                                 <div class="bottomfooterwidgety">
                                                     <h3>SFC GRIP</h3>
-                                                    <p>　ホーム　|　サイトマップ　|　コンセプト　|　運営主体　|　お問い合わせ </p>
+                                                    <div id="footnav">
+<ul>
+    <li><a href="#">ホーム</a></li>
+    <li>|</li>
+    <li><a href="#">サイトマップ</a></li>
+    <li>|</li>
+    <li><a href="#">コンセプト</a></li>
+    <li>|</li>
+    <li><a href="#">運営主体</a></li>
+    <li>|</li>
+    <li><a href="#">お問い合わせ</a></li>
+</ul> 
+</div>
                                                         <div class="footerp"><?php _e('Copyright &copy; 2014 , SFC GRIP All rights reserved.', 'hujuu') ?></div>
                                                         <?php if( is_home() || is_front_page() ): ?>
                                                         <?php else: ?>
